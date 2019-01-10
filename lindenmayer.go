9c3f85c16763fade7f4f846dc9363e3c1306918d
Nikola Trubitsyn <nikola.trubitsyn@gmail.com>
2019-01-11 01:50:02 +0300

    Initial implementation

--- lindenmayer.go
+++ lindenmayer.go
@@ -1,1 +1,32 @@
+// Copyright 2019 Nikola Trubitsyn. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
 package lindenmayer
+
+import (
+	"strings"
+)
+
+type LSystem struct {
+	Variables []rune
+	Constants []rune
+	Axiom rune
+	Rules []Rule
+	Angle float64
+}
+
+type Rule struct {
+	In string
+	Out string
+}
+
+func Iterate(lsystem LSystem, max int, f func(string)) {
+	c := string(lsystem.Axiom)
+	for i := 0; i < max; i++ {
+		for _, rule := range lsystem.Rules {
+			c = strings.Replace(c, rule.In, rule.Out, -1)
+		}
+		f(c)
+	}
+}
--- lindenmayer_test.go
+++ lindenmayer_test.go
@@ -1,1 +1,25 @@
+// Copyright 2019 Nikola Trubitsyn. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
 package lindenmayer
+
+import (
+	"testing"
+)
+
+func TestExample(t *testing.T) {
+	sys := LSystem {
+		Variables: []rune{'F'},
+		Constants: []rune{'+', '-'},
+		Axiom: 'F',
+		Rules: []Rule{
+			Rule{In: "F", Out: "F+F−F−F+F"},
+		},
+	}
+	Iterate(sys, 1, func (s string) {
+		if (s != "F+F−F−F+F") {
+			t.Fail()
+		}
+	})
+}
